55eceecd6f153d06e08a56ed009fac726c1eca1a
Ethan Fung <EthanEFung@gmail.com>
2018-01-04 23:41:10 -0800

    test: detail tests for door security

--- specs/routes.vehicles.test.js
+++ specs/routes.vehicles.test.js
@@ -30,22 +30,46 @@ describe('the vehicle router', () => {
     it('should send a driveTrain from GM');
   });
 
+  describe('/:id/doors', () => {
+    //request from client
+    it('should receive a request from the client');
+    it('should receive the id of the client in the params');
+
+    //request to GM
+    it('should send a request to GM');
+    it('should request a location for each door from GM');
+    it('should request a "locked status" for each door from GM');
+
+    //response from GM
+    it('should receive a response from GM API');
+    it('should receive a location for each door from GM');
+    it('should receive a "locked status" for each door from GM');
+
+    //response to client
+    it('should send a response back to the client');
+    it('should send a location for each door from GM');
+    it('should send a "locked status" for each door from GM');
+    it('should send an array')
+  });
+
   describe('/:id/fuel', () => {
     it('should receive a request from the client');
     it('should send a request to GM API');
     it('should receive a response from GM API');
     it('should send a response back to the client');
-  })
+  });
+
   describe('/:id/battery', () => {
     it('should receive a request from the client')
     it('should send a request to GM API');
     it('should receive a response from GM API');
     it('should send a response back to the client');
-  })
+  });
+
   describe('/:id/engine', () => {
     it('should receive a request from the client')
     it('should send a request to GM API');
     it('should receive a response from GM API');
     it('should send a response back to the client');
-  })
+  });
 })
