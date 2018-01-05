0a76becb73f6a9d505309ccdf25ed2876c440a31
Ethan Fung <EthanEFung@gmail.com>
2018-01-05 08:51:16 -0800

    docs: detail params for vehicle info controller

--- server/controllers/vehicles/vehicleInfo.js
+++ server/controllers/vehicles/vehicleInfo.js
@@ -1,3 +1,34 @@
+// it('should receive a GET request from the client');
+// it('should receive the id of the client in the params');
+
+// //request to GM
+// it('should send a POST request to GM');
+// it('should send the id of the client in the requests body');
+// it('should specify a responseType of "JSON" in the requests body');
+
+// //response from GM
+// it('should receive a response from GM API');
+// it('should receive a "data" attribute')
+// it('should receive a vin number from GM');
+// it('should receive a color from GM');
+// it('should receive a "fourDoorSedan" status from GM');
+// it('should receive a "twoDoorCoupe" status from GM');
+// it('should receive a driveTrain from GM');
+
+// //response to client
+// it('should send a response back to the client');
+// it('should send a vin number');
+// it('should send a color');
+// it('should send a doorCount');
+// it('should send a driveTrain');
+// it('should send a single object')
+
+/**
+ * route controller that handles GET requests for a given information.
+ * Sends to the client a JSON object that 
+ * @param {{ params: { id: number } }} req - a readable stream that contains an id param
+ * @param {{ body: { vin: string, color: string, doorCount: number, driveTrain: string } }} res - a writeable stream that is sent to the client
+ */
 function vehicleInfo(req, res) {
 
 }
