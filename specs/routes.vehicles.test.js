c758f0ebc0f076dde4fcd2981ef0995aea29b98f
Ethan Fung <EthanEFung@gmail.com>
2018-01-05 00:12:55 -0800

    test: expand on the req and res of GM for Vehicle Info

--- specs/routes.vehicles.test.js
+++ specs/routes.vehicles.test.js
@@ -9,17 +9,17 @@ describe('the vehicle router', () => {
     it('should receive the id of the client in the params');
 
     //request to GM
-    it('should send a request to GM');
-    it('should request a vin number from GM');
-    it('should request a color from GM');
-    it('should request a doorCount from GM');
-    it('should request a driveTrain from GM');
+    it('should send a POST request to GM');
+    it('should send the id of the client in the requests body');
+    it('should specify a responseType of "JSON" in the requests body');
 
     //response from GM
     it('should receive a response from GM API');
+    it('should receive a "data" attribute')
     it('should receive a vin number from GM');
     it('should receive a color from GM');
-    it('should receive a doorCount from GM');
+    it('should receive a "fourDoorSedan" status from GM');
+    it('should receive a "twoDoorCoupe" status from GM')
     it('should receive a driveTrain from GM');
 
     //response to client
