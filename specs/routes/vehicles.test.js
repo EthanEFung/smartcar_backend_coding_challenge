f1a3a1638b25f922043e683b7083a78eef276cd2
Ethan Fung <EthanEFung@gmail.com>
2018-01-05 13:59:41 -0800

    refactor: move vehicleInfo GM request to controller unit tests

--- specs/routes/vehicles.test.js
+++ specs/routes/vehicles.test.js
@@ -4,20 +4,6 @@ describe('the vehicle router', () => {
     it('should receive a GET request from the client');
     it('should receive the id of the client in the params');
 
-    //request to GM
-    it('should send a POST request to GM');
-    it('should send the id of the client in the requests body');
-    it('should specify a responseType of "JSON" in the requests body');
-
-    //response from GM
-    it('should receive a response from GM API');
-    it('should receive a "data" attribute');
-    it('should receive a vin number from GM');
-    it('should receive a color from GM');
-    it('should receive a "fourDoorSedan" status from GM');
-    it('should receive a "twoDoorCoupe" status from GM');
-    it('should receive a driveTrain from GM');
-
     //response to client
     it('should send a response back to the client');
     it('should send a vin number');
