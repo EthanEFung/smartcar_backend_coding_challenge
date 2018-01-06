2027f0f06818609fa6eba16c6620457f6431338d
Ethan Fung <EthanEFung@gmail.com>
2018-01-05 19:21:59 -0800

    feat: create all purpose GM fetch Promise
    Created a modular Promise that makes a get request to the GM API when given the correct dependencies

---
+++ server/helpers/fetchGMData.js
@@ -0,0 +1,11 @@
+function fetchGMData(path, init, id) {
+  console.log('fetching...')
+  return new Promise((resolve, reject) => {
+    fetch(path, init)
+      .then(res => res.json())
+      .then(data => resolve(data))
+      .catch(err => reject(err));
+  });
+}
+
+module.exports = fetchGMData;
