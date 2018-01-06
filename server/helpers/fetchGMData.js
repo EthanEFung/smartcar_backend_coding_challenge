583fbb8a4a2c09560db8b8a84a4c58fa766df01d
Ethan Fung <EthanEFung@gmail.com>
2018-01-06 13:46:37 -0800

    docs: correct documentation

--- server/helpers/fetchGMData.js
+++ server/helpers/fetchGMData.js
@@ -3,11 +3,11 @@ const handleGMErrors = require('./handleGMErrors');
 
 /**
  * Promise that makes a POST request to the GM API to support
- * GM specifications for GET and POST requests.
+ * GM specifications for GET and POST requests, and handles GM errors.
  * Returns a promise with a GM Response.
  * IMPORTANT TO NOTE: GM API does not `throw` errors. Response is sent back
  * containing a status code that must be parsed and handled
- * synchronously on our servers. Use this fetch function to handle GM errors
+ * synchronously on our servers. Use this fetch function to handle GM errors.
  * @param {string} path
  * @param {{ headers: {}, method: string, body: JSON }} init
  * @param {number} id 
