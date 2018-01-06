40be027005637d08ab9c17532867d8691a733fdb
Ethan Fung <EthanEFung@gmail.com>
2018-01-06 15:15:22 -0800

    docs: remove specification comments from vehicleInfo

--- server/controllers/vehicles/vehicleInfo.js
+++ server/controllers/vehicles/vehicleInfo.js
@@ -2,32 +2,6 @@ const Promise = require('promise-polyfill');
 const fetchGMData = require('../../helpers/fetchGMData');
 const handleGMErrors = require('../../helpers/handleGMErrors');
 
-// //request from client
-// it should receive a GET request from the client
-// it should receive the id of the client in the params
-
-// //request to GM
-// it should send a POST request to GM
-// it should send the id of the client in the requests body
-// it should specify a responseType of "JSON" in the requests body
-
-// //response from GM
-// it should receive a response from GM API
-// it should receive a "data" attribute
-// it should receive a vin number from GM
-// it should receive a color from GM
-// it should receive a "fourDoorSedan" status from GM
-// it should receive a "twoDoorCoupe" status from GM
-// it should receive a driveTrain from GM
-
-// //response to client
-// it should send a response back to the client
-// it should send a vin number
-// it should send a color
-// it should send a doorCount
-// it should send a driveTrain
-// it should send a single object
-
 /**
  * Route controller that handles GET requests for a vehicles information.
  * Sends to the client an object that contains the following:
