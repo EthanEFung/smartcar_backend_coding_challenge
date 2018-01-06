1a03049b0ab66b68f49c3b684fb8d3163613a15a
Ethan Fung <EthanEFung@gmail.com>
2018-01-05 22:05:51 -0800

    refactor: simplify error handler

--- server/helpers/handleGMErrors.js
+++ server/helpers/handleGMErrors.js
@@ -1,17 +1,12 @@
 
 /**
- * TODO
+ * 
  * @param {*} response 
  */
 function handleGMErrors(response) {
-  if (response.status[0] === '3') {
-    throw new Error('Redirection');
-  }
-  if (response.status[0] === '4') {
-    throw new Error('Client Error');
-  }
-  if (response.status[0] === '5') {
-    throw new Error('GM Server Error');
+  const type = response.status[0];
+  if (type >= '3' && type <= '5') {
+    throw response;
   }
 }
 
