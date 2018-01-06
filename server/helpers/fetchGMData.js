8d4923969fadc1e7c9dd0db636c4292942665db7
Ethan Fung <EthanEFung@gmail.com>
2018-01-06 12:36:03 -0800

    refactor: render fetchGMData that supports error handling

--- server/helpers/fetchGMData.js
+++ server/helpers/fetchGMData.js
@@ -1,9 +1,13 @@
 const fetch = require('node-fetch');
+const handleGMErrors = require('./handleGMErrors');
 
 /**
- * Promise that makes a POST request to the GM API.
+ * Promise that makes a POST request to the GM API to support
+ * GM specifications for GET and POST requests.
  * Returns a promise with a GM Response.
- * To Note: GM API does not throw errors
+ * IMPORTANT TO NOTE: GM API does not `throw` errors. Response is sent back
+ * containing a status code that must be parsed and handled
+ * synchronously on our servers. Use this fetch function to handle GM errors
  * @param {string} path
  * @param {{ headers: {}, method: string, body: JSON }} init
  * @param {number} id 
@@ -14,8 +18,15 @@ function fetchGMData(path, init, id) {
   return new Promise((resolve, reject) => {
     fetch(path, init)
       .then(res => res.json())
-      .then(data => resolve(data))
-      .catch(err => reject(err));
+      .then(data => {
+        try {
+          handleGMErrors(data);
+          resolve(data);
+        } catch (e) {
+          reject(e);
+        }
+      })
+    // .catch(err => reject(err));
   });
 }
 
