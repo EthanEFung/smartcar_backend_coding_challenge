d86104339237d9bebbb3804ea8a9235db89881fd
Ethan Fung <EthanEFung@gmail.com>
2018-01-05 22:31:27 -0800

    feat: render fuelRange controller

--- server/controllers/vehicles/fuelRange.js
+++ server/controllers/vehicles/fuelRange.js
@@ -1,3 +1,61 @@
-function fuelRange(req, res) { }
+const Promise = require('promise-polyfill');
+const fetchGMData = require('../../helpers/fetchGMData');
+const handleGMErrors = require('../../helpers/handleGMErrors');
+// //request from client
+// should receive a GET request from the client
+// should receive the id of the client in the params
 
-module.exports = fuelRange;
+// //request to GM
+// should send a POST request to GM
+// should specify the clients id in the request body
+// should specify a responseType of "JSON" in the request body
+
+// //response from GM
+// should receive a response object from GM API
+// should contain a data object
+// should have a tankLevel in the data object
+// should have a batteryLevel in  the data object
+// should have a value specified for the tankLevel and the batteryLevel
+
+// //response to client
+// should send a response back to the client
+// should send the percentage of fuel left
+// should send an object
+
+function fuelRange(req, res) {
+  console.log(`request has been made for vehicle #${req.params.id} fuel range`)
+  const path = `https://gmapi.azurewebsites.net/getEnergyService`;
+  const init = {
+    headers: { 'Content-Type': 'application/json' },
+    method: 'POST',
+    body: JSON.stringify({
+      id: req.params.id,
+      responseType: 'JSON'
+    })
+  }
+
+  fetchGMData(path, init, req.params.id)
+    .then(processGMFuelRangeData)
+    .then(data => res.send(data))
+    .catch(err => res.send(err));
+}
+
+function processGMFuelRangeData(response) {
+  console.log('processing...');
+  return new Promise((resolve, reject) => {
+    try {
+      handleGMErrors(response);
+      const { value } = response.data.tankLevel
+      resolve({
+        percent: parseInt(value)
+      });
+      console.log('OK: sending JSON to client');
+    } catch (e) {
+      console.log('Err: could not processes response');
+      reject(e);
+    }
+  });
+}
+
+
+module.exports = { fuelRange, processGMFuelRangeData };
