1815c693303a2619271a6f12a5d3ab6f00030a5a
Ethan Fung <EthanEFung@gmail.com>
2018-01-06 13:08:58 -0800

    feat: add useful bodyParser middleware functionality
    added urlencoded and json middleware to support requests with json, and url encoded bodies

--- server/index.js
+++ server/index.js
@@ -9,6 +9,8 @@ const app = express();
 const port = process.env.PORT || 8000;
 const router = require('./routes/index.js');
 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
 app.use(router);
 
 app.listen(port, err => {
