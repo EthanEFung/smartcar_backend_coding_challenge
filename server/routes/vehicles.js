f80c2f516125e90312d3e19404bcd7ceccb9656c
Ethan Fung <EthanEFung@gmail.com>
2018-01-06 12:03:40 -0800

    refactor: support engineAction unit testing

--- server/routes/vehicles.js
+++ server/routes/vehicles.js
@@ -6,7 +6,7 @@ const { vehicleInfo } = require('../controllers/vehicles/vehicleInfo');
 const { security } = require('../controllers/vehicles/security');
 const { fuelRange } = require('../controllers/vehicles/fuelRange');
 const { batteryRange } = require('../controllers/vehicles/batteryRange');
-const engineAction = require('../controllers/vehicles/engineAction');
+const { engineAction } = require('../controllers/vehicles/engineAction');
 
 router
   .get('/:id', vehicleInfo)
