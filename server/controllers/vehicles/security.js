7f5dc6ead8ca48d83487c3977b28ea161c1422a3
Ethan Fung <EthanEFung@gmail.com>
2018-01-06 15:07:28 -0800

    docs: correct and expand security controller docs

--- server/controllers/vehicles/security.js
+++ server/controllers/vehicles/security.js
@@ -2,37 +2,14 @@ const Promise = require('promise-polyfill');
 const fetchGMData = require('../../helpers/fetchGMData');
 const handleGMErrors = require('../../helpers/handleGMErrors');
 
-// //request from client
-// it should receive a GET request from the client'
-// it should receive the id of the client in the params'
-
-// //request to GM
-// it should send a POST request to GM'
-// it should send the clients id in the body of the request to GM'
-// it should specify the responseType to be JSON in the body of the request'
-
-// //response from GM
-// it should receive a response from GM API'
-// it should receive a "data" object from GM'
-// it has the "doors" object nested in the data object'
-// it has the "values" array nested in the doors object'
-// it has door objects for each door, which store location and locked'
-// it has a value attribute for each door location'
-// it has a value attribute for each door\'s "locked" status
-
-// //response to client
-// it should send a response back to the client'
-// it should send an array to the client'
-// it should send an object for each door location to the client'
-// it should send a "locked status" for each door to the client'
-// it should send a location for each door to the client'
-
 /**
- * A route handler that receives requests for the
- * the locations and "locked" statuses of a specified cars doors
- * and sends to the client these points of emphasis
+ * A route controller that receives GET requests for the
+ * the `location` and `locked` statuses of a specified car's doors
+ * and sends via the `res` parameter's `send` method a JSON array 
+ * of objects containing these door statuses to the client.
+ * Controller sends GM a POST request as detailed in GM specifications.
  * @param {{ param: { id: number } }} req 
- * @param {[{ location: string, locked: boolean }, { location: string, locked: boolean }]} res 
+ * @param {{ send: function }} res 
  */
 function security(req, res) {
   try {
