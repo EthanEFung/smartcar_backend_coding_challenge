4d6e0be1d0084c9bd2f39b5ec96a00b421eb67fa
Ethan Fung <EthanEFung@gmail.com>
2018-01-06 14:14:57 -0800

    refactor: add try and catch blocks to fuelRange

--- server/controllers/vehicles/fuelRange.js
+++ server/controllers/vehicles/fuelRange.js
@@ -23,36 +23,48 @@ const handleGMErrors = require('../../helpers/handleGMErrors');
 // should send an object
 
 function fuelRange(req, res) {
-  console.log(`request has been made for vehicle #${req.params.id} fuel range`)
-  const path = `https://gmapi.azurewebsites.net/getEnergyService`;
-  const init = {
-    headers: { 'Content-Type': 'application/json' },
-    method: 'POST',
-    body: JSON.stringify({
-      id: req.params.id,
-      responseType: 'JSON'
-    })
-  }
+  try {
+    console.log(`request has been made for vehicle #${req.params.id} fuel range`)
+    const path = `https://gmapi.azurewebsites.net/getEnergyService`;
+    const init = {
+      headers: { 'Content-Type': 'application/json' },
+      method: 'POST',
+      body: JSON.stringify({
+        id: req.params.id,
+        responseType: 'JSON'
+      })
+    }
 
-  fetchGMData(path, init, req.params.id)
-    .then(processGMFuelRangeData)
-    .then(data => res.send(data))
-    .catch(err => res.send(err));
+    fetchGMData(path, init, req.params.id)
+      .then(processGMFuelRangeData)
+      .then(data => res.send(data))
+      .catch(err => res.send(err));
+  } catch (e) {
+    res.send({
+      client_message: 'Error on our end! We need to update our server to our chagrin.',
+      status: 500,
+      error: 'Internal Error:\n' + e
+    })
+    throw e;
+  }
 }
 
 function processGMFuelRangeData(response) {
   console.log('processing...');
   return new Promise((resolve, reject) => {
     try {
-      handleGMErrors(response);
       const { value } = response.data.tankLevel
       resolve({
         percent: parseInt(value)
       });
       console.log('OK: sending JSON to client');
     } catch (e) {
-      console.log('Err: could not processes response');
-      reject(e);
+      const internalErr = {
+        client_message: 'Error on our end! We need to update our server to our chagrin.',
+        status: 500,
+        error: e
+      };
+      throw internalErr;
     }
   });
 }
