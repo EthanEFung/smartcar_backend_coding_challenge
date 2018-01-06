4c124851047b2c716efc5f10ea62c49654947745
Ethan Fung <EthanEFung@gmail.com>
2018-01-05 22:38:35 -0800

    bug: fix routing issue with batteryRange

--- server/routes/vehicles.js
+++ server/routes/vehicles.js
@@ -5,7 +5,7 @@ const router = require('express').Router();
 const { vehicleInfo } = require('../controllers/vehicles/vehicleInfo');
 const { security } = require('../controllers/vehicles/security');
 const { fuelRange } = require('../controllers/vehicles/fuelRange');
-const batteryRange = require('../controllers/vehicles/batteryRange');
+const { batteryRange } = require('../controllers/vehicles/batteryRange');
 const engineAction = require('../controllers/vehicles/engineAction');
 
 router
