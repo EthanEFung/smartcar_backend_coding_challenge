97d83d0bacedf62d9ed778adccaedc3adcaefca2
Ethan Fung <EthanEFung@gmail.com>
2018-01-06 17:11:52 -0800

    chore: test callback style error handler
    testing passing two seperate callbacks in handleErrors

--- server/helpers/handleErrors.beta.js
+++ server/helpers/handleErrors.beta.js
@@ -1,5 +1,5 @@
 /**
- * STILL IN BETA & BUGGY CURRENTLY DEPRECATED
+ * STILL IN BETA. DO NOT USE
  * An internal error handler. Provide this function with a `res`
  * response stream to send to clients, and a `callback` that performs
  * the desired processes. Upon an error, this function sends notification
@@ -9,15 +9,16 @@
  * @param {{ send: function }} res 
  * @param {function} callback 
  */
-function handleErrors(res, callback) {
+function handleErrors(reject, callback) {
   try {
     callback();
   } catch (e) {
     const internalErr = {
       client_message: 'Error on our end! We need to update our server to our chagrin.',
       status: 500,
+      error: e
     };
-    res.send(internalErr);
+    reject(internalErr);
     throw internalErr;
   }
 }
