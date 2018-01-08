aa7d5e0dd375a0ff2ea76a7c3d65996a8fbf5db8
Ethan Fung <EthanEFung@gmail.com>
2018-01-08 12:49:12 -0800

    test: create tests for engineAction functionality

---
+++ specs/controllers/vehicles/engineAction.test.js
@@ -0,0 +1,172 @@
+import {
+  engineAction,
+  parseEngineActionRequest,
+  parseEngineActionResponse,
+  processGMEngineActionData
+} from '../../../server/controllers/vehicles/engineAction';
+
+describe('engineAction functionality', () => {
+  it('should be tested');
+});
+
+describe('parseEngineActionRequest', () => {
+  it('should return "START_VEHICLE" if the action is "START"', () => {
+    expect(parseEngineActionRequest('START')).toBe('START_VEHICLE');
+  });
+
+  it('should return "STOP_VEHICLE" if the action is "STOP"', () => {
+    expect(parseEngineActionRequest('STOP')).toBe('STOP_VEHICLE');
+  });
+
+  it('should throw if the action has the word start or stop but is not formatted', () => {
+    expect.assertions(1);
+    try {
+      parseEngineActionRequest('START ');
+    } catch (e) {
+      expect(e).toEqual({
+        error: "Bad Request: submit 'START' or 'STOP' as an action in the body of your JSON",
+        status: 400
+      });
+    }
+  });
+});
+
+describe('processGMEngineActionData', () => {
+  it('should resolve if given a correctly formatted response', () => {
+    expect.assertions(2);
+
+    const test = {
+      actionResult: {
+        status: 'EXECUTED'
+      }
+    }
+
+    processGMEngineActionData(test)
+      .then(data => expect(data).toEqual({ status: 'success' }))
+
+    const test2 = {
+      actionResult: {
+        status: 'FAILED'
+      }
+    }
+
+    return processGMEngineActionData(test2)
+      .then(data => expect(data).toEqual({ status: 'error' }))
+  });
+
+  it('resolves with an object', () => {
+    expect.assertions(1);
+    const test = {
+      actionResult: {
+        status: 'EXECUTED'
+      }
+    }
+
+    return processGMEngineActionData(test)
+      .then(data => expect(typeof data).toBe('object'));
+  });
+
+  it('resolves with a status', () => {
+    expect.assertions(1);
+    const test = {
+      actionResult: {
+        status: 'EXECUTED'
+      }
+    }
+
+    return processGMEngineActionData(test)
+      .then(data => expect(data.status).toBeTruthy());
+  });
+
+  it('resolves with a status of "success" if passed "EXECUTED"', () => {
+    expect.assertions(1);
+    const test = {
+      actionResult: {
+        status: 'EXECUTED'
+      }
+    }
+
+    return processGMEngineActionData(test)
+      .then(data => expect(data.status).toBe('success'));
+  });
+
+  it('resolves with a status of "error" if passed "FAILED"', () => {
+    expect.assertions(1);
+    const test = {
+      actionResult: {
+        status: 'FAILED'
+      }
+    }
+
+    return processGMEngineActionData(test)
+      .then(data => expect(data.status).toBe('error'));
+  });
+
+  it('should throw an error if the GM response is empty', () => {
+    expect.assertions(1);
+    const internalErr = new Error(
+      JSON.stringify({
+        "client_message": "Error on our end! We need to update our server to our chagrin.",
+        "error": TypeError(`Cannot read property 'status' of undefined`),
+        "status": 500
+      })
+    )
+
+    return processGMEngineActionData({})
+      .catch(err => expect(err).toEqual(internalErr));
+  });
+
+  it('should throw an error if the GM response is doesnt have an actionResult obj', () => {
+    expect.assertions(1);
+    const internalErr = new Error(
+      JSON.stringify({
+        "client_message": "Error on our end! We need to update our server to our chagrin.",
+        "error": TypeError(`Cannot read property 'status' of undefined`),
+        "status": 500
+      })
+    )
+
+    return processGMEngineActionData({ someOtherAttr: {} })
+      .catch(err => expect(err).toEqual(internalErr));
+  });
+
+  it('should throw an error if the actionResult object is doesnt have a status', () => {
+    expect.assertions(1);
+    const internalErr = new Error(
+      JSON.stringify({
+        "client_message": "Error on our end! We need to update our server to our chagrin.",
+        "error": `GM formatting error`,
+        "status": 500
+      })
+    )
+
+    return processGMEngineActionData({
+      actionResult: {
+        type: "String"
+      }
+    })
+      .catch(err => expect(err).toEqual(internalErr));
+  });
+})
+
+describe('parseEngineActionResponse', () => {
+  it('should return "success" if the status is "EXECUTED"', () => {
+    expect(parseEngineActionResponse('EXECUTED')).toBe('success');
+  });
+
+  it('should return "error" if the action is "FAILED"', () => {
+    expect(parseEngineActionResponse('FAILED')).toBe('error');
+  });
+
+  it('should throw if the action has the word start or stop but is not formatted', () => {
+    try {
+      parseEngineActionResponse('EXECUTEDS');
+    } catch (e) {
+      expect(e).toEqual({
+        client_message: 'Error on our end! We need to update our server to our chagrin.',
+        status: 500,
+        error: `status needs to be updated to support "EXECUTEDS"`
+      });
+    }
+  });
+})
